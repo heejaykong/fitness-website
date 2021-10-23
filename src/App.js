bd66cdf96ad48600b340fa2d8765e0160858bc46
heejaykong <heejay.dev@gmail.com>
2021-10-23 18:02:47 +0900

    refactor: remove useless comments && stories page && refactoring

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import React, { useState, useEffect } from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./routes/Home";
-import Stories from "./routes/Stories";
-// import Footer from './components/Footer';
 import { ThemeProvider } from "styled-components/macro";
 import GlobalStyle from "./css/globalStyle";
 import theme from "./css/theme";
 
 function App() {
   const [width, setWidth] = useState(window.innerWidth);
-  
+  const [isMobile, setIsMobile] = useState(null);
+
   useEffect(() => {
     const handleWindowSizeChange = () => setWidth(window.innerWidth);
     window.addEventListener("resize", handleWindowSizeChange);
+    setIsMobile(width <= theme.BREAKPOINT);
 
     return () => {
       window.removeEventListener("resize", handleWindowSizeChange);
     };
-  });
-
-  const isMobile = width <= theme.BREAKPOINT;
+  }, [width]);
 
   return (
     <>
@@ -34,13 +32,8 @@ function App() {
               exact={true}
               component={() => <Home isMobile={isMobile} />}
             />
-            <Route
-              path="/stories"
-              component={() => <Stories isMobile={isMobile} />}
-            />
           </Switch>
         </HashRouter>
-        {/* <Footer /> */}
       </ThemeProvider>
     </>
   );
